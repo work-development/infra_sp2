8defa20caf6006be78ae6771d6fa439cefbbfbbc
Aleshin <aleshincc@yandex.ru>
2020-09-15 23:26:14 +0300

    Project: docker-compose version 5

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import environ
+env = environ.Env()
+environ.Env.read_env()
 from datetime import timedelta
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
